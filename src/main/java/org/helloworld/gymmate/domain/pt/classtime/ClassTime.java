57072b94fce2e8621dafc3f04a27ce80b1f9c971
daydreaam <dayingyeo@naver.com>
2025-03-27 17:08:11 +0900

    feat(pt) : make entity /GYMMATE-46"

---
+++ src/main/java/org/helloworld/gymmate/domain/pt/classtime/ClassTime.java
@@ -0,0 +1,33 @@
+package org.helloworld.gymmate.domain.pt.classtime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "class_time")
+public class ClassTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "class_time_id", nullable = false)
+	private Long classTimeId;
+
+	@Column(name = "time", nullable = false)
+	private int time;
+
+	@Column(name = "day_of_week", nullable = false)
+	private int dayOfWeek;
+}
