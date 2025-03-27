3dd96d40a583f49b988d3e854ea4c6ff157ca306
daydreaam <dayingyeo@naver.com>
2025-03-27 18:46:48 +0900

    for merge

--- src/main/java/org/helloworld/gymmate/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/helloworld/gymmate/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,6 @@
 package org.helloworld.gymmate.common.exception;
 
-import java.util.stream.Collectors;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,19 +9,6 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<ErrorDetails> handleBusinessException(BusinessException e) {
 		return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(ErrorDetails.of(e));
-	}
-
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorDetails> handleValidationException(MethodArgumentNotValidException ex) {
-		// 필드별 에러 메시지 생성
-		String errorMessage = ex.getBindingResult().getFieldErrors()
-			.stream()
-			.map(error -> error.getField() + ": " + error.getDefaultMessage())
-			.collect(Collectors.joining(", "));
-
-		ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
-		return ResponseEntity.status(errorCode.getHttpStatus())
-			.body(new ErrorDetails(errorCode.getHttpStatus(), errorCode.getErrorCode(), errorMessage));
 	}
 
 	@ExceptionHandler(Exception.class)
