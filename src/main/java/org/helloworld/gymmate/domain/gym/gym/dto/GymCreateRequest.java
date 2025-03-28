4de197cdf9388425df144f64d37ef2349bc0a93c
seeyeon <gulf72pr@swu.ac.kr>
2025-03-28 10:23:06 +0900

    feat: dto 관련 추가

---
+++ src/main/java/org/helloworld/gymmate/domain/gym/gym/dto/GymCreateRequest.java
@@ -0,0 +1,35 @@
+package org.helloworld.gymmate.domain.gym.gym.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record GymCreateRequest (
+	@NotBlank(message = "헬스장 이름을 입력해주세요.")
+	String gymName,
+
+	@NotBlank(message = "운영 시작 시간을 입력해주세요.")
+	String startTime,
+
+	@NotBlank(message = "운영 종료 시간을 입력해주세요.")
+	String endTime,
+
+	@NotBlank(message = "전화번호를 입력해주세요.")
+	String phoneNumber,
+
+	@NotBlank(message = "주소를 입력해주세요.")
+	String address,
+
+	@NotBlank(message = "x 좌표를 입력해주세요.")
+	String xField,
+
+	@NotBlank(message = "y 좌표를 입력해주세요.")
+	String yField,
+
+	@NotBlank(message = "헬스장 소개를 입력해주세요.")
+	String intro,
+
+	List<String> imageUrls
+){
+
+}
---
+++ src/main/java/org/helloworld/gymmate/domain/gym/gym/mapper/GymMapper.java
@@ -0,0 +1,37 @@
+package org.helloworld.gymmate.domain.gym.gym.mapper;
+
+import java.util.List;
+
+import org.helloworld.gymmate.domain.gym.gym.dto.GymCreateRequest;
+import org.helloworld.gymmate.domain.gym.gym.entity.Gym;
+import org.helloworld.gymmate.domain.gym.gym.entity.GymImage;
+
+public class GymMapper {
+
+	public static Gym toEntity(GymCreateRequest request){
+		Gym gym = Gym.builder()
+			.gymName(request.gymName())
+			.startTime(request.startTime())
+			.endTime(request.endTime())
+			.phoneNumber(request.phoneNumber())
+			.isPartner(true)
+			.address(request.address())
+			.xField(request.xField())
+			.yField(request.yField())
+			.intro(request.intro())
+			.avgScore(0.0)
+			.build();
+
+		if (request.imageUrls() != null && !request.imageUrls().isEmpty()) {
+			List<GymImage> imageEntities = request.imageUrls().stream()
+				.map(url -> GymImage.builder().url(url).build())
+				.toList();
+
+			gym.addImages(imageEntities);
+		}
+
+		return gym;
+	}
+
+
+}
