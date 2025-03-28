481c6fa4eec580baef0067c34d3ec621f292f2b8
seeyeon <gulf72pr@swu.ac.kr>
2025-03-28 17:30:26 +0900

    feat(gym): ErrorCode 추가

--- src/main/java/org/helloworld/gymmate/common/exception/ErrorCode.java
+++ src/main/java/org/helloworld/gymmate/common/exception/ErrorCode.java
@@ -33,7 +33,24 @@ public enum ErrorCode {
 	VOTE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "VOTE-002", "이미 투표한 내역이 존재합니다."),
 	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "PARAMETER-001", "요청 파라미터가 유효하지 않습니다."),
 	CLASSTIME_DUPLICATED(HttpStatus.NOT_FOUND, "PT-CLASSTIME-001", "이미 등록된 수업 시간입니다."),
-	CLASSTIME_NOT_FOUND(HttpStatus.NOT_FOUND, "PT-CLASSTIME-002", "존재하지 않는 PT 수강 가능 시간입니다.");
+	CLASSTIME_NOT_FOUND(HttpStatus.NOT_FOUND, "PT-CLASSTIME-002", "존재하지 않는 PT 수강 가능 시간입니다."),
+
+	//gym
+	GYM_INVALID_REQUEST(HttpStatus.BAD_REQUEST, "GYM-001", "잘못된 입력값입니다."),
+	GYM_NOT_FOUND(HttpStatus.NOT_FOUND, "GYM-002", "해당 헬스장을 찾을 수 없습니다."),
+	GYM_ALREADY_EXISTS(HttpStatus.CONFLICT, "GYM-003", "이미 등록된 헬스장입니다."),
+	GYM_REGISTRATION_FORBIDDEN(HttpStatus.FORBIDDEN, "GYM-004", "헬스장 등록 권한이 없습니다."),
+	GYM_FORBIDDEN(HttpStatus.FORBIDDEN, "GYM-005", "해당 헬스장에 대한 수정 권한이 없습니다."),
+
+	// 인증 관련
+	AUTH_REQUIRED(HttpStatus.UNAUTHORIZED, "AUTH-001", "인증이 필요합니다. 로그인 후 다시 시도해주세요."),
+
+	// 이미지 관련
+	IMAGE_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, "IMAGE-002", "업로드한 파일 크기가 너무 큽니다. 최대 5MB까지 가능합니다."),
+	IMAGE_UNSUPPORTED_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "IMAGE-003", "지원되지 않는 파일 형식입니다. (jpg, png, gif만 가능)"),
+
+	// S3 관련
+	S3_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3-001", "이미지 업로드 중 오류가 발생했습니다. 다시 시도해주세요.");
 
 	private final HttpStatus httpStatus;
 	private final String errorCode;
