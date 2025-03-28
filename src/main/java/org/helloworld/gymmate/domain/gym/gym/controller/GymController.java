764d432f3b8ed1aafe72ed27ac5b502ee08b5797
seeyeon <gulf72pr@swu.ac.kr>
2025-03-28 15:30:23 +0900

    feat(gym): S3 적용을 위해 코드 일부 수정

--- src/main/java/org/helloworld/gymmate/domain/gym/gym/controller/GymController.java
+++ src/main/java/org/helloworld/gymmate/domain/gym/gym/controller/GymController.java
@@ -1,17 +1,21 @@
 package org.helloworld.gymmate.domain.gym.gym.controller;
 
-import java.util.Map;
+import java.util.List;
 
 import org.helloworld.gymmate.domain.gym.gym.dto.GymCreateRequest;
 import org.helloworld.gymmate.domain.gym.gym.dto.GymResponse;
 import org.helloworld.gymmate.domain.gym.gym.service.GymService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.validation.Valid;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,11 +24,17 @@ import lombok.RequiredArgsConstructor;
 public class GymController {
 
 	private final GymService gymService;
+	private final ObjectMapper objectMapper;
 
-	@PostMapping
-	public ResponseEntity<GymResponse> createGym
-		(@RequestBody @Valid GymCreateRequest request){
-			GymResponse response = gymService.createGym(request);
-			return ResponseEntity.ok(response);
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<GymResponse> createGym(
+		@RequestPart("request") String requestJson,
+		@RequestPart("images") List<MultipartFile> images)throws JsonProcessingException {
+
+		// JSON 문자열을 record로 변환
+		GymCreateRequest request = objectMapper.readValue(requestJson, GymCreateRequest.class);
+
+		GymResponse response = gymService.createGym(request, images);
+		return ResponseEntity.ok(response);
 	}
 }
--- src/main/java/org/helloworld/gymmate/domain/gym/gym/service/GymService.java
+++ src/main/java/org/helloworld/gymmate/domain/gym/gym/service/GymService.java
@@ -1,5 +1,8 @@
 package org.helloworld.gymmate.domain.gym.gym.service;
 
+import java.util.List;
+
+import org.helloworld.gymmate.common.s3.FileManager;
 import org.helloworld.gymmate.domain.gym.gym.dto.GymCreateRequest;
 import org.helloworld.gymmate.domain.gym.gym.dto.GymResponse;
 import org.helloworld.gymmate.domain.gym.gym.entity.Gym;
@@ -7,6 +10,7 @@ import org.helloworld.gymmate.domain.gym.gym.mapper.GymMapper;
 import org.helloworld.gymmate.domain.gym.gym.repository.GymRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,10 +19,26 @@ import lombok.RequiredArgsConstructor;
 public class GymService {
 
 	private final GymRepository gymRepository;
+	private final FileManager fileManager;
 
 	@Transactional
-	public GymResponse createGym(GymCreateRequest request) {
-		Gym gym = GymMapper.toEntity(request);
+	public GymResponse createGym(GymCreateRequest request, List<MultipartFile> images) {
+
+		List<String> imageUrls = fileManager.uploadFiles(images, "gymImage");
+
+		GymCreateRequest updatedRequest = new GymCreateRequest(
+			request.gymName(),
+			request.startTime(),
+			request.endTime(),
+			request.phoneNumber(),
+			request.address(),
+			request.xField(),
+			request.yField(),
+			request.intro(),
+			imageUrls
+		);
+
+		Gym gym = GymMapper.toEntity(updatedRequest);
 		Gym saved = gymRepository.save(gym);
 		return GymMapper.toResponse(saved);
 	}
