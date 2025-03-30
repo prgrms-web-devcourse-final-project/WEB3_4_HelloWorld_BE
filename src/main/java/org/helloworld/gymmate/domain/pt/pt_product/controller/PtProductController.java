e63b86f352236a458d86217406db798d8b1d7dbc
daydreaam <dayingyeo@naver.com>
2025-03-30 14:14:12 +0900

    feat(ptProduct) : make getProducts

--- src/main/java/org/helloworld/gymmate/domain/pt/pt_product/controller/PtProductController.java
+++ src/main/java/org/helloworld/gymmate/domain/pt/pt_product/controller/PtProductController.java
@@ -3,16 +3,21 @@ package org.helloworld.gymmate.domain.pt.pt_product.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.helloworld.gymmate.common.dto.PageDto;
+import org.helloworld.gymmate.common.mapper.PageMapper;
 import org.helloworld.gymmate.common.validate.custom.ValidImageFile;
 import org.helloworld.gymmate.domain.pt.pt_product.dto.PtProductCreateRequest;
 import org.helloworld.gymmate.domain.pt.pt_product.dto.PtProductModifyRequest;
+import org.helloworld.gymmate.domain.pt.pt_product.dto.PtProductsResponse;
 import org.helloworld.gymmate.domain.pt.pt_product.service.PtProductService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -47,6 +52,18 @@ public class PtProductController {
 
 		return ResponseEntity.ok(
 			Map.of("ptClassId",ptProductService.modifyPtProduct(productId, request, images)));
+	}
+
+	@GetMapping
+	public ResponseEntity<PageDto<PtProductsResponse>> getProducts(
+		@RequestParam(defaultValue = "score") String sortOption,
+		@RequestParam String searchOption,
+		@RequestParam String searchTerm,
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int pageSize
+	){
+		return ResponseEntity.ok(PageMapper.toPageDto(
+			ptProductService.getProducts(sortOption, searchOption, searchTerm, page, pageSize)));
 	}
 
 }
