03781e6cfb27034c34e2fb530459407f2a8aa463
dia218 <dia19dev@gmail.com>
2025-03-31 01:01:41 +0900

    feat(myself): Implement modifyRecord & getRecords /GYMMATE-172 /GYMMATE-174

--- src/main/java/org/helloworld/gymmate/domain/myself/record/service/RecordService.java
+++ src/main/java/org/helloworld/gymmate/domain/myself/record/service/RecordService.java
@@ -7,9 +7,13 @@ import org.helloworld.gymmate.domain.myself.record.dto.RecordCreateRequest;
 import org.helloworld.gymmate.domain.myself.record.dto.RecordModifyRequest;
 import org.helloworld.gymmate.domain.myself.record.dto.RecordResponse;
 import org.helloworld.gymmate.domain.myself.record.entity.Record;
+import org.helloworld.gymmate.domain.myself.record.mapper.RecordMapper;
 import org.helloworld.gymmate.domain.myself.record.repository.RecordRepository;
 import org.helloworld.gymmate.domain.user.member.entity.Member;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -23,33 +27,44 @@ public class RecordService {
         // 날짜가 비어있으면 현재 시간 넣기
         LocalDate date = request.date() != null ? request.date() : LocalDate.now();
 
-        Record record = Record.builder()
-                .date(date)
-                .title(request.title())
-                .content(request.content())
-                .member(member)
-                .build();
-
-        recordRepository.save(record);
+        recordRepository.save(RecordMapper.toEntity(request, member, date));
     }
 
     public void deleteRecord(Long recordId, Member member) {
-        Record record = recordRepository.findById(recordId)
-                .orElseThrow(() -> new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR)); //TODO: 에러코드 수정
+        Record existRecord = getExistingRecord(recordId);
 
-        validateRecordOwner(record, member);
+        validateRecordOwner(existRecord, member);
 
-        recordRepository.delete(record);
+        recordRepository.delete(existRecord);
     }
 
     public void modifyRecord(Long recordId, RecordModifyRequest request, Member member) {
+        Record existRecord = getExistingRecord(recordId);
+
+        validateRecordOwner(existRecord, member);
+
+        // 기존 날짜 가져오기
+        LocalDate date = existRecord.getDate();
+
+        recordRepository.save(RecordMapper.toEntity(request, member, date));
     }
 
     public Page<RecordResponse> getRecords(int page, int size, Member member) {
-        return null;
+        Pageable pageable = PageRequest.of(page, size, Sort.by("date").descending());
+
+        Page<Record> recordPage = recordRepository.findByMember(member, pageable);
+
+        return recordPage.map(RecordMapper::toDto);
+    }
+
+    private Record getExistingRecord(Long recordId) {
+        //기존 운동 기록 가져오기
+        return recordRepository.findById(recordId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.RECORD_NOT_FOUND));
     }
 
     private void validateRecordOwner(Record record, Member member) {
+        // 본인의 운동 기록이 아니면 예외 발생
         if (record.getMember().equals(member)) {
             throw new BusinessException(ErrorCode.USER_NOT_AUTHORIZED);
         }
