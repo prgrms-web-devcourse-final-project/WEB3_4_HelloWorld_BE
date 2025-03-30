87e185c24884511267b7900fe9e9bb94c10743eb
yuunha <h000414@naver.com>
2025-03-30 17:28:31 +0900

    refactor(member): userDetail관련해 GymmateUser->member

--- src/main/java/org/helloworld/gymmate/security/authentication/GymmateUserDetailsService.java
+++ src/main/java/org/helloworld/gymmate/security/authentication/GymmateUserDetailsService.java
@@ -1,5 +1,9 @@
 package org.helloworld.gymmate.security.authentication;
 
+import org.helloworld.gymmate.common.exception.BusinessException;
+import org.helloworld.gymmate.common.exception.ErrorCode;
+import org.helloworld.gymmate.domain.user.member.entity.Member;
+import org.helloworld.gymmate.domain.user.member.repository.MemberRepository;
 import org.helloworld.gymmate.domain.user.service.GymmateUserService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,14 +15,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class GymmateUserDetailsService implements UserDetailsService {
 
-	private final GymmateUserService gymmateUserService;
+	private final MemberRepository memberRepository;
 
 	@Override
-	public GymmateUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return new GymmateUserDetails(gymmateUserService.findByNickName(username));
+	public GymmateUserDetails loadUserByUsername(String username){
+		Member member = memberRepository.findByMemberName(username)
+			.orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND)
+			);
+
+		return new GymmateUserDetails(member);
 	}
 
 	public GymmateUserDetails loadUserByUserId(Long userId) {
-		return loadUserByUsername(gymmateUserService.findNickNameByUserId(userId));
+		return loadUserByUsername(
+			memberRepository.findMemberNameByMemberId(userId)
+			.orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND))
+		);
 	}
 }
