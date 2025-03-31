9c99a2f1315b6b9ad3132569b37e7f57d62739d1
segye <tprpek@gmail.com>
2025-03-31 14:16:35 +0900

    chore(member): 주석 해제

--- src/main/java/org/helloworld/gymmate/domain/user/member/mapper/MemberMapper.java
+++ src/main/java/org/helloworld/gymmate/domain/user/member/mapper/MemberMapper.java
@@ -1,16 +1,19 @@
 package org.helloworld.gymmate.domain.user.member.mapper;
 
+import org.helloworld.gymmate.domain.user.member.dto.MemberRequest;
+import org.helloworld.gymmate.domain.user.member.entity.Member;
+
 public class MemberMapper {
 
-	// public static Member toMember(SocialProvider socialProvider, MemberRequest memberRequest) {
-	// 	return Member.builder()
-	// 		.phoneNumber(String.valueOf(memberRequest.getPhoneNumber()))
-	// 		.memberName(memberRequest.getMemberName())
-	// 		.birthday(memberRequest.getBirthday())
-	// 		.email(memberRequest.getEmail())
-	// 		.genderType(memberRequest.getGenderType())
-	// 		.socialProvider(socialProvider)
-	// 		.build();
-	// }
+	public static Member toMember(SocialProvider socialProvider, MemberRequest memberRequest) {
+		return Member.builder()
+			.phoneNumber(String.valueOf(memberRequest.getPhoneNumber()))
+			.memberName(memberRequest.getMemberName())
+			.birthday(memberRequest.getBirthday())
+			.email(memberRequest.getEmail())
+			.genderType(memberRequest.getGenderType())
+			.socialProvider(socialProvider)
+			.build();
+	}
 
 }
--- src/main/java/org/helloworld/gymmate/domain/user/member/service/MemberService.java
+++ src/main/java/org/helloworld/gymmate/domain/user/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package org.helloworld.gymmate.domain.user.member.service;
 
+import org.helloworld.gymmate.domain.user.enums.SocialProviderType;
+import org.helloworld.gymmate.domain.user.member.dto.MemberRequest;
+import org.helloworld.gymmate.domain.user.member.entity.Member;
+import org.helloworld.gymmate.domain.user.member.mapper.MemberMapper;
 import org.helloworld.gymmate.domain.user.member.repository.MemberRepository;
 import org.helloworld.gymmate.security.oauth.repository.OauthRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,14 +18,15 @@ public class MemberService {
 	private final MemberRepository memberRepository;
 	private final OauthRepository oauthRepository;
 
-	// @Transactional
-	// public Long createMember(SocialProviderType socialProviderType, String socialProviderId, MemberRequest memberRequest) {
-	// 	SocialProvider socialProvider = SocialProviderMapper.toEntity(socialProviderType, socialProviderId);
-	// 	socialProviderRepository.save(socialProvider);
-	//
-	// 	Member member = MemberMapper.toMember(socialProvider,memberRequest);
-	//
-	// 	return memberRepository.save(member).getMemberId();
-	// }
+	@Transactional
+	public Long createMember(SocialProviderType socialProviderType, String socialProviderId,
+		MemberRequest memberRequest) {
+		SocialProvider socialProvider = SocialProviderMapper.toEntity(socialProviderType, socialProviderId);
+		socialProviderRepository.save(socialProvider);
+
+		Member member = MemberMapper.toMember(socialProvider, memberRequest);
+
+		return memberRepository.save(member).getMemberId();
+	}
 
 }
