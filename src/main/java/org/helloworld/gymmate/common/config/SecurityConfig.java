3ef391a7dd21fc6a21227a3e7a44cab9dd209fe2
daydreaam <dayingyeo@naver.com>
2025-03-31 17:51:41 +0900

    refactor(gym) : gym crawling v1 refactor

--- src/main/java/org/helloworld/gymmate/common/config/SecurityConfig.java
+++ src/main/java/org/helloworld/gymmate/common/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
 				"/v3/**", "/swagger-ui/**", "/api/logistics",
 				"h2-console/**", "/error",
 				// 이하는 개발환경에서만 허용해야 함
-				"/gym/**", "/ptProduct/**", "/classtime"
+				"/gym/**", "/ptProduct/**", "/classtime", "/crawl/**"
 			)
 			//.requestMatchers(HttpMethod.GET, "/similar")
 			//.requestMatchers(HttpMethod.GET, "/reviews/{reviewId}/comments")
