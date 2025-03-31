4345f6447dc7eb847f6384da14a8a5cf4969b3c4
dia218 <dia19dev@gmail.com>
2025-03-31 15:49:44 +0900

    feat(myself): Implement CreateBigthree /GYMMATE-149

--- src/main/java/org/helloworld/gymmate/domain/myself/bigthree/controller/BigthreeController.java
+++ src/main/java/org/helloworld/gymmate/domain/myself/bigthree/controller/BigthreeController.java
@@ -1,9 +1,19 @@
 package org.helloworld.gymmate.domain.myself.bigthree.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.helloworld.gymmate.domain.myself.bigthree.dto.BigthreeCreateRequest;
 import org.helloworld.gymmate.domain.myself.bigthree.service.BigthreeService;
+import org.helloworld.gymmate.domain.user.member.entity.Member;
+import org.helloworld.gymmate.security.oauth.entity.CustomOAuth2User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/bigthree")
@@ -11,4 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BigthreeController {
     private BigthreeService bigthreeService;
 
+    @PostMapping
+    public ResponseEntity<Map<String, Long>> createRecord(
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+            @Valid @RequestBody BigthreeCreateRequest request) {
+
+        Member member = null; //TODO: memberService.findByUserId(customOAuth2User.getUserId());
+        long bigthreeId = bigthreeService.createBigthree(request, member);
+
+        return ResponseEntity.ok(
+                Map.of("bigthreeId", bigthreeId));
+    }
 }
--- src/main/java/org/helloworld/gymmate/domain/myself/bigthree/service/BigthreeService.java
+++ src/main/java/org/helloworld/gymmate/domain/myself/bigthree/service/BigthreeService.java
@@ -1,12 +1,26 @@
 package org.helloworld.gymmate.domain.myself.bigthree.service;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.helloworld.gymmate.domain.myself.bigthree.dto.BigthreeCreateRequest;
+import org.helloworld.gymmate.domain.myself.bigthree.mapper.BigthreeMapper;
 import org.helloworld.gymmate.domain.myself.bigthree.repository.BigthreeRepository;
+import org.helloworld.gymmate.domain.user.member.entity.Member;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class BigthreeService {
     private BigthreeRepository bigthreeRepository;
-    
+
+    @Transactional
+    public long createBigthree(@Valid BigthreeCreateRequest request, Member member) {
+        // 날짜가 비어있으면 현재 시간 넣기
+        LocalDate date = request.date() != null ? request.date() : LocalDate.now();
+
+        return bigthreeRepository.save(BigthreeMapper.toEntity(request, member, date)).getBigthreeId();
+    }
 }
