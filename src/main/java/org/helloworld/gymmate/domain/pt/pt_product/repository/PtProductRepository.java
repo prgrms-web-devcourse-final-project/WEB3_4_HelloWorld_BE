264845c170a08b102ad50a03f5cd49744c6324bd
segye <tprpek@gmail.com>
2025-03-31 17:43:50 +0900

    feat(trainer): 트레이너 추가 정보 등록 구현 /GYMMATE-223 직원은 헬스장 이름을 요청으로 받고 해당 헬스장을 찾은 후 연결해주는 방식으로 구현했고 사장은 사업자 등록번호 검증 로직을 추가해 구현하였습니다

--- src/main/java/org/helloworld/gymmate/domain/pt/pt_product/repository/PtProductRepository.java
+++ src/main/java/org/helloworld/gymmate/domain/pt/pt_product/repository/PtProductRepository.java
@@ -12,67 +12,68 @@ public interface PtProductRepository extends JpaRepository<PtProduct, Long> {
 	/*
 		fetchLatestProducts 
 	 */
-	
+
 	Page<PtProduct> findAllByOrderByPtProductIdDesc(Pageable pageable);
 
 	@Query("""
-        SELECT p FROM PtProduct p
-        JOIN Trainer t ON p.trainerId = t.trainerId
-        WHERE t.trainerName LIKE %:searchTerm%
-        ORDER BY p.ptProductId DESC
-    """)
+		    SELECT p FROM PtProduct p
+		    JOIN Trainer t ON p.trainerId = t.trainerId
+		    WHERE t.trainerName LIKE %:searchTerm%
+		    ORDER BY p.ptProductId DESC
+		""")
 	Page<PtProduct> findByTrainerNameOrderByPtProductIdDesc(@Param("searchTerm") String searchTerm, Pageable pageable);
-	
+
 	@Query("""
-    SELECT p FROM PtProduct p
-    WHERE p.info LIKE %:searchTerm%
-    ORDER BY p.ptProductId DESC
-    """)
-	Page<PtProduct> findByPtProductNameOrderByPtProductIdDesc(@Param("searchTerm") String searchTerm, Pageable pageable);
-	
+		SELECT p FROM PtProduct p
+		WHERE p.info LIKE %:searchTerm%
+		ORDER BY p.ptProductId DESC
+		""")
+	Page<PtProduct> findByPtProductNameOrderByPtProductIdDesc(@Param("searchTerm") String searchTerm,
+		Pageable pageable);
+
 	@Query("""
-        SELECT p FROM PtProduct p
-        JOIN Trainer t ON p.trainerId = t.trainerId
-        JOIN Gym g ON t.gymId = g.gymId
-        WHERE g.address LIKE %:searchTerm%
-        ORDER BY p.ptProductId DESC
-    """)
+		    SELECT p FROM PtProduct p
+		    JOIN Trainer t ON p.trainerId = t.trainerId
+		    JOIN Gym g ON t.gym.gymId = g.gymId
+		    WHERE g.address LIKE %:searchTerm%
+		    ORDER BY p.ptProductId DESC
+		""")
 	Page<PtProduct> findByGymAddressOrderByPtProductIdDesc(@Param("searchTerm") String searchTerm, Pageable pageable);
 
 	/*
 		fetchScoreSortedProducts
 	 */
-	
+
 	@Query("""
-		SELECT p FROM PtProduct p
-		JOIN Trainer t ON p.trainerId = t.trainerId
-		ORDER BY t.score DESC
-	""")
+			SELECT p FROM PtProduct p
+			JOIN Trainer t ON p.trainerId = t.trainerId
+			ORDER BY t.score DESC
+		""")
 	Page<PtProduct> findAllOrderByTrainerScoreDesc(Pageable pageable);
 
 	@Query("""
-		SELECT p FROM PtProduct p
-		JOIN Trainer t ON p.trainerId = t.trainerId
-		WHERE t.trainerName LIKE %:searchTerm%
-		ORDER BY t.score DESC
-	""")
+			SELECT p FROM PtProduct p
+			JOIN Trainer t ON p.trainerId = t.trainerId
+			WHERE t.trainerName LIKE %:searchTerm%
+			ORDER BY t.score DESC
+		""")
 	Page<PtProduct> findByTrainerNameOrderByScoreDesc(@Param("searchTerm") String searchTerm, Pageable pageable);
 
 	@Query("""
-        SELECT p FROM PtProduct p
-        JOIN Trainer t ON p.trainerId = t.trainerId
-        WHERE p.info LIKE %:searchTerm%
-        ORDER BY t.score DESC
-    """)
+		    SELECT p FROM PtProduct p
+		    JOIN Trainer t ON p.trainerId = t.trainerId
+		    WHERE p.info LIKE %:searchTerm%
+		    ORDER BY t.score DESC
+		""")
 	Page<PtProduct> findByPtProductNameOrderByScoreDesc(@Param("searchTerm") String searchTerm, Pageable pageable);
 
 	@Query("""
-        SELECT p FROM PtProduct p
-        JOIN Trainer t ON p.trainerId = t.trainerId
-        JOIN Gym g ON t.gymId = g.gymId
-        WHERE g.address LIKE %:searchTerm%
-        ORDER BY t.score DESC
-    """)
+		    SELECT p FROM PtProduct p
+		    JOIN Trainer t ON p.trainerId = t.trainerId
+		    JOIN Gym g ON t.gym.gymId = g.gymId
+		    WHERE g.address LIKE %:searchTerm%
+		    ORDER BY t.score DESC
+		""")
 	Page<PtProduct> findByGymAddressOrderByScoreDesc(@Param("searchTerm") String searchTerm, Pageable pageable);
 
 	/*
