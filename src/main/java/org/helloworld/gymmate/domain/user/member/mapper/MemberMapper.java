d1ba8afd62f33353f0e1c1b7168f2e4efd479f93
yuunha <h000414@naver.com>
2025-03-31 12:45:32 +0900

    refactor(member):충돌 해결(socialProvider->oauth)

--- src/main/java/org/helloworld/gymmate/domain/user/member/mapper/MemberMapper.java
+++ src/main/java/org/helloworld/gymmate/domain/user/member/mapper/MemberMapper.java
@@ -1,20 +1,19 @@
 package org.helloworld.gymmate.domain.user.member.mapper;
 
-import org.helloworld.gymmate.domain.user.member.dto.MemberRequest;
+import org.helloworld.gymmate.domain.user.enums.GenderType;
 import org.helloworld.gymmate.domain.user.member.entity.Member;
-import org.helloworld.gymmate.domain.user.model.SocialProvider;
-import org.helloworld.gymmate.domain.user.trainer.model.Trainer;
+import org.helloworld.gymmate.security.oauth.entity.Oauth;
 
 public class MemberMapper {
 
-	public static Member toMember(SocialProvider socialProvider, MemberRequest memberRequest) {
+	public static Member ToMember(Oauth oauth){
 		return Member.builder()
-			.phoneNumber(String.valueOf(memberRequest.getPhoneNumber()))
-			.memberName(memberRequest.getMemberName())
-			.birthday(memberRequest.getBirthday())
-			.email(memberRequest.getEmail())
-			.genderType(memberRequest.getGenderType())
-			.socialProvider(socialProvider)
+			.oauth(oauth)
+			.phoneNumber("010-1111-2222")
+			.memberName("이름1")
+			.email("aa@naver.com")
+			.genderType(GenderType.FEMALE)
+			.birthday("2000-00-00")
 			.build();
 	}
 
