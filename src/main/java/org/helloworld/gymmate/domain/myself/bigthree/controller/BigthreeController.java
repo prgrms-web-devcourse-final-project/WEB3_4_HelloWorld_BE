3186aa6c34256a2753209ac1b24dae53120da485
dia218 <dia19dev@gmail.com>
2025-03-31 16:25:20 +0900

    feat(myself): Implement DeleteBigthree /GYMMATE-192

--- src/main/java/org/helloworld/gymmate/domain/myself/bigthree/controller/BigthreeController.java
+++ src/main/java/org/helloworld/gymmate/domain/myself/bigthree/controller/BigthreeController.java
@@ -2,8 +2,16 @@ package org.helloworld.gymmate.domain.myself.bigthree.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.helloworld.gymmate.domain.myself.bigthree.service.BigthreeService;
+import org.helloworld.gymmate.domain.user.member.entity.Member;
+import org.helloworld.gymmate.security.oauth.entity.CustomOAuth2User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/bigthree")
@@ -11,4 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class BigthreeController {
     private BigthreeService bigthreeService;
 
+
+    @DeleteMapping(value = "/{bigthreeId}")
+    public ResponseEntity<Map<String, Long>> deleteBigthree(
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+            @PathVariable Long bigthreeId) {
+
+        Member member = null; //TODO: memberService.findByUserId(customOAuth2User.getUserId());
+        bigthreeService.deleteBigthree(bigthreeId, member);
+
+        return ResponseEntity.ok().build();
+    }
+    
 }
--- src/main/java/org/helloworld/gymmate/domain/myself/bigthree/service/BigthreeService.java
+++ src/main/java/org/helloworld/gymmate/domain/myself/bigthree/service/BigthreeService.java
@@ -1,12 +1,23 @@
 package org.helloworld.gymmate.domain.myself.bigthree.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.helloworld.gymmate.domain.myself.bigthree.entity.Bigthree;
 import org.helloworld.gymmate.domain.myself.bigthree.repository.BigthreeRepository;
+import org.helloworld.gymmate.domain.user.member.entity.Member;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BigthreeService {
     private BigthreeRepository bigthreeRepository;
-    
+
+    @Transactional
+    public void deleteBigthree(Long bigthreeId, Member member) {
+        Bigthree existBigthree = getExistingBigthree(bigthreeId);
+
+        validateBigthreeOwner(existBigthree, member);
+
+        bigthreeRepository.delete(existBigthree);
+    }
 }
