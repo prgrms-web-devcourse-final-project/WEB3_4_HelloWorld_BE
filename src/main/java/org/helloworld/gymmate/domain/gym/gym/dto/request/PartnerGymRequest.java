72b985b5dddc46912dde15eaa4a8fad9709cffd7
seeyeon <gulf72pr@swu.ac.kr>
2025-04-01 18:37:40 +0900

    feat(gym): PartnerGym Dto 추가

---
+++ src/main/java/org/helloworld/gymmate/domain/gym/gym/dto/request/PartnerGymRequest.java
@@ -0,0 +1,4 @@
+package org.helloworld.gymmate.domain.gym.gym.dto.request;
+
+public record PartnerGymRequest(Long gymId) {
+}
---
+++ src/main/java/org/helloworld/gymmate/domain/gym/gym/dto/response/PartnerGymResponse.java
@@ -0,0 +1,4 @@
+package org.helloworld.gymmate.domain.gym.gym.dto.response;
+
+public record PartnerGymResponse(Long partnerGymId) {
+}
