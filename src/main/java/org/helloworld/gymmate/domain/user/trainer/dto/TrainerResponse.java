ad9dd707bae8254a395e9e9d613f83bf80c0e113
segye <tprpek@gmail.com>
2025-04-01 22:18:19 +0900

    feat(trainer) : 트레이너 마이페이지 조회, 사장 여부 체크 구현 트레이너 마이페이지도 필요 하다고 하셔서 만들었고 사장 여부 체크는 마이페이지로 접속할때 화면이 모두 달라서 멤버인지 트레이너인지 체크하고 트레이너라면 사장인지 체크해야한다고 하셨습니다

--- src/main/java/org/helloworld/gymmate/domain/user/trainer/dto/TrainerResponse.java
+++ src/main/java/org/helloworld/gymmate/domain/user/trainer/dto/TrainerResponse.java
@@ -2,6 +2,15 @@ package org.helloworld.gymmate.domain.user.trainer.dto;
 
 public record TrainerResponse(
 	Long trainerId,
-	String trainerName
+	String trainerName,
+	String bank,
+	String account,
+	String gender,
+	String profile,
+	Boolean isOwner,
+	Long cash,
+	String intro,
+	String career,
+	String field
 ) {
 }
