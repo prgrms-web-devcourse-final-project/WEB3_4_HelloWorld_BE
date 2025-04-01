2b4a0eb132baf80f5e824f5c04d64e40f9166c13
seeyeon <gulf72pr@swu.ac.kr>
2025-04-01 18:38:30 +0900

    refactor(gym): 제휴 헬스장 등록 방식 변경

--- src/main/java/org/helloworld/gymmate/domain/gym/gym/controller/GymController.java
+++ src/main/java/org/helloworld/gymmate/domain/gym/gym/controller/GymController.java
@@ -2,15 +2,20 @@ package org.helloworld.gymmate.domain.gym.gym.controller;
 
 import java.util.List;
 
-import org.helloworld.gymmate.domain.gym.gym.dto.GymCreateRequest;
-import org.helloworld.gymmate.domain.gym.gym.dto.GymResponse;
-import org.helloworld.gymmate.domain.gym.gym.dto.GymUpdateRequest;
+import org.helloworld.gymmate.domain.gym.gym.dto.request.GymUpdateRequest;
+import org.helloworld.gymmate.domain.gym.gym.dto.request.PartnerGymRequest;
+import org.helloworld.gymmate.domain.gym.gym.dto.response.GymResponse;
+import org.helloworld.gymmate.domain.gym.gym.dto.response.PartnerGymResponse;
 import org.helloworld.gymmate.domain.gym.gym.service.GymService;
+import org.helloworld.gymmate.security.oauth.entity.CustomOAuth2User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,13 +31,15 @@ public class GymController {
 
 	private final GymService gymService;
 
-	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ResponseEntity<GymResponse> createGym(
-		@RequestPart("request") @Valid GymCreateRequest request,
-		@RequestPart("images") List<MultipartFile> images){
+	//제휴 헬스장 등록
+	@PostMapping
+	public ResponseEntity<PartnerGymResponse> registerPartnerGym(
+		@RequestBody PartnerGymRequest request,
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User){
 
-		GymResponse response = gymService.createGym(request, images);
-		return ResponseEntity.ok(response);
+		Long ownerId = customOAuth2User.getUserId(); // 인증된 사용자 ID
+		PartnerGymResponse response = gymService.registerPartnerGym(request, ownerId);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
 	@PutMapping(value = "/{gymId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
