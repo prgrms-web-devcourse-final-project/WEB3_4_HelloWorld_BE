a487ffba91d37d01590a40930d1b052413f81fa0
dia218 <dia19dev@gmail.com>
2025-04-01 18:07:36 +0900

    fix(myself): Change CREATE Return

--- src/main/java/org/helloworld/gymmate/domain/myself/bigthree/controller/BigthreeController.java
+++ src/main/java/org/helloworld/gymmate/domain/myself/bigthree/controller/BigthreeController.java
@@ -6,11 +6,10 @@ import org.helloworld.gymmate.domain.myself.bigthree.dto.BigthreeCreateRequest;
 import org.helloworld.gymmate.domain.myself.bigthree.dto.BigthreeRequest;
 import org.helloworld.gymmate.domain.myself.bigthree.service.BigthreeService;
 import org.helloworld.gymmate.security.oauth.entity.CustomOAuth2User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/bigthree")
@@ -22,11 +21,8 @@ public class BigthreeController {
     public ResponseEntity<Long> createBigthree(
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
             @Valid @RequestBody BigthreeCreateRequest request) {
-
-        long bigthreeId = bigthreeService.createBigthree(request, customOAuth2User.getUserId());
-
-        URI location = URI.create("/bigthree/" + bigthreeId);
-        return ResponseEntity.created(location).body(bigthreeId);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(bigthreeService.createBigthree(request, customOAuth2User.getUserId()));
     }
 
     @DeleteMapping(value = "/{bigthreeId}")
