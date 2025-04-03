4d0a1bd5924bf668e8cb4278b6e73896c3bbfe8a
daydreaam <dayingyeo@naver.com>
2025-04-03 13:57:35 +0900

    feat(trainer) : security config

--- src/main/java/org/helloworld/gymmate/common/config/SecurityConfig.java
+++ src/main/java/org/helloworld/gymmate/common/config/SecurityConfig.java
@@ -75,6 +75,7 @@ public class SecurityConfig {
 				"/crawl/**"
 			)
 			.requestMatchers(HttpMethod.GET, "/ptProduct", "/ptProduct/{id:\\d+}")
+			.requestMatchers(HttpMethod.GET, "/trainer/list")
 			//.requestMatchers(HttpMethod.GET, "/similar")
 			//.requestMatchers(HttpMethod.GET, "/reviews/{reviewId}/comments")
 			.requestMatchers(PathRequest.toH2Console());
