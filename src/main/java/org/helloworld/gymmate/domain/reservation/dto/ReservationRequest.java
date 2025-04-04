da90eb5147f54aba7aedc24ff780c207da56d3b1
yuunha <h000414@naver.com>
2025-04-04 14:26:09 +0900

    refactor(reservation):requestDto 필요없는 정보 제거

--- src/main/java/org/helloworld/gymmate/domain/reservation/dto/ReservationRequest.java
+++ src/main/java/org/helloworld/gymmate/domain/reservation/dto/ReservationRequest.java
@@ -4,15 +4,10 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
 public record ReservationRequest(
 	Long reservationId,
-
-	@NotBlank(message = "상품명은 필수입니다")
-	String productName,
 
 	@NotNull(message = "예약 날짜는 필수입니다")
 	LocalDate date,
@@ -21,10 +16,6 @@ public record ReservationRequest(
 	@Min(value = 0, message = "예약 시간은 0시 이상이어야 합니다")
 	@Max(value = 23, message = "예약 시간은 23시 이하여야 합니다")
 	Integer time,
-
-	@NotNull(message = "가격은 필수입니다")
-	@Positive(message = "가격은 0보다 커야 합니다")
-	Long price,
 
 	LocalDate cancelDate, // 예약 생성 시에는 필요 없음
 
