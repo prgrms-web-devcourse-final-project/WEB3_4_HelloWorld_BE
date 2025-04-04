8f0e2631f22bacc8272882f14b962b16147a1655
seeyeon <gulf72pr@swu.ac.kr>
2025-04-04 15:52:18 +0900

    feat(gymProduct): 임시 저장

--- src/main/java/org/helloworld/gymmate/domain/gym/gymInfo/dto/request/UpdateGymRequest.java
+++ src/main/java/org/helloworld/gymmate/domain/gym/gymInfo/dto/request/UpdateGymRequest.java
@@ -2,8 +2,12 @@ package org.helloworld.gymmate.domain.gym.gymInfo.dto.request;
 
 import java.util.List;
 
+import org.helloworld.gymmate.domain.gym.gymProduct.dto.GymProductRequest;
+
 public record UpdateGymRequest(
+	Long partnerGymId,
 	GymInfoRequest gymInfoRequest,
+	List<GymProductRequest> gymProductRequest,
 	List<Long> deleteImageIds
 ) {
 }
