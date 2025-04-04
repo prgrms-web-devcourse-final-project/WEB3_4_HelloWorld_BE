41d88cacc331094543cc0f969777f3cb83248b22
daydreaam <dayingyeo@naver.com>
2025-04-04 12:51:40 +0900

    feat(gym) : fake machine url update

--- src/main/java/org/helloworld/gymmate/domain/gym/machine/enums/GymMachine.java
+++ src/main/java/org/helloworld/gymmate/domain/gym/machine/enums/GymMachine.java
@@ -9,19 +9,30 @@ import lombok.Getter;
 
 @Getter
 public enum GymMachine {
-	BENCH_PRESS("벤치 프레스", "s3://your-bucket/bench_press.jpg"),
-	SQUAT_RACK("스쿼트 랙", "s3://your-bucket/squat_rack.jpg"),
-	LAT_PULLDOWN("랫 풀다운", "s3://your-bucket/lat_pulldown.jpg"),
-	LEG_PRESS("레그 프레스", "s3://your-bucket/leg_press.jpg"),
-	CHEST_PRESS("체스트 프레스", "s3://your-bucket/chest_press.jpg"),
-	PEC_DECK("펙덱 머신", "s3://your-bucket/pec_deck.jpg"),
-	SHOULDER_PRESS("숄더 프레스", "s3://your-bucket/shoulder_press.jpg"),
-	LEG_EXTENSION("레그 익스텐션", "s3://your-bucket/leg_extension.jpg"),
-	LEG_CURL("레그 컬", "s3://your-bucket/leg_curl.jpg"),
-	CABLE_MACHINE("케이블 머신", "s3://your-bucket/cable_machine.jpg"),
-	DUMBBELLS("덤벨 세트", "s3://your-bucket/dumbbells.jpg"),
-	BARBELLS("바벨 세트", "s3://your-bucket/barbells.jpg"),
-	TREADMILL("러닝머신", "s3://your-bucket/treadmill.jpg");
+	BENCH_PRESS("벤치 프레스",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/73e7696d-186a-4e35-991f-f478819b0709.jpg"),
+	SQUAT_RACK("스쿼트 랙",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/1018ce3b-0787-429e-9e79-6acb8c3f19d0.jpg"),
+	LAT_PULLDOWN("랫 풀다운",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/870dda50-fbc9-49fd-a9a8-49bf42cd2a27.jpg"),
+	LEG_PRESS("레그 프레스",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/6e4e4557-8278-4508-a0c0-fbb80c62eb35.jpg"),
+	CHEST_PRESS("체스트 프레스",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/6b0588c5-0de7-43b6-8bc0-5cfc3845db50.jpg"),
+	PEC_DECK("펙덱 머신",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/5ff0d849-4465-4ef5-8ebc-e7c9db9ebc42.jpg"),
+	SHOULDER_PRESS("숄더 프레스",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/88f06b71-f1b8-4270-91d6-53b7f9c33a26.jpg"),
+	LEG_EXTENSION("레그 익스텐션",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/b8c76ab5-d076-4c14-94cb-2996029021cb.jpg"),
+	LEG_CURL("레그 컬",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/9492d250-8619-4b2d-adaf-72c046f82000.jpg"),
+	CABLE_MACHINE("케이블 머신",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/9162c463-16ad-4445-bb9e-2a39202a6916.jpg"),
+	BARBELLS("바벨 세트",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/d4cc38e5-3c78-42b5-befe-62b3dc4e34db.jpg"),
+	TREADMILL("러닝머신",
+		"https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/machine/20250404/76e1ad7d-7306-4644-911f-579d72dff967.jpg");
 
 	private final String name;
 	private final String imageUrl;
