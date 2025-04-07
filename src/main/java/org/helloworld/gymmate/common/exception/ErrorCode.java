0ac1809988e302b1615eb3442a8edfc67e1e1195
daydreaam <dayingyeo@naver.com>
2025-04-07 15:52:16 +0900

    feat(gym) : merging main

--- src/main/java/org/helloworld/gymmate/common/exception/ErrorCode.java
+++ src/main/java/org/helloworld/gymmate/common/exception/ErrorCode.java
@@ -43,7 +43,6 @@ public enum ErrorCode {
 	// GymProduct
 	GYMPRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "GYM-PRODUCT-001", "존재하지 않는 헬스장 이용권입니다."),
 	GYMPRODUCT_PARTNER_MISMATCH(HttpStatus.BAD_REQUEST, "GYM-PRODUCT-002", "해당 이용권은 이 파트너 헬스장에 속하지 않습니다."),
-	GYMPRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "GYM-PRODUCT-001", "존재하지 않는 헬스장 상품입니다."),
 
 	// GymTicket
 	GYM_TICKET_NOT_FOUND(HttpStatus.NOT_FOUND, "GYM-TICKET-001", "존재하지 않는 헬스장 이용권입니다."),
