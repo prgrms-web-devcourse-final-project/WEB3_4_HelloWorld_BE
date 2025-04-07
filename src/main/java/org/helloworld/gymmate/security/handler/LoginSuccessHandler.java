3b3e0bc32ec55c6f9e93aa3bd785dfb26e4d4526
dia218 <dia19dev@gmail.com>
2025-04-07 15:06:20 +0900

    chore(security): Tap 공백 취소

--- src/main/java/org/helloworld/gymmate/security/handler/LoginSuccessHandler.java
+++ src/main/java/org/helloworld/gymmate/security/handler/LoginSuccessHandler.java
@@ -29,65 +29,65 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-	private final CookieManager cookieManager;
-	private final JwtManager jwtManager;
-	private final ExpirationPolicy expirationPolicy;
-	private final TrainerService trainerService;
-	private final MemberService memberService;
-	@Value("${jwt.redirect}")
-	private String REDIRECT_URI; // 메인 페이지로 리다이렉트
-	@Value("${jwt.redirect-login}")
-	private String LOGIN_REDIRECT_URI; // 로그인 페이지로 리다이렉트
+    private final CookieManager cookieManager;
+    private final JwtManager jwtManager;
+    private final ExpirationPolicy expirationPolicy;
+    private final TrainerService trainerService;
+    private final MemberService memberService;
+    @Value("${jwt.redirect}")
+    private String REDIRECT_URI; // 메인 페이지로 리다이렉트
+    @Value("${jwt.redirect-login}")
+    private String LOGIN_REDIRECT_URI; // 로그인 페이지로 리다이렉트
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-		Authentication authentication) throws IOException {
-		OAuth2AuthenticationToken token = (OAuth2AuthenticationToken)authentication; // 토큰
-		CustomOAuth2User customOAuth2User = (CustomOAuth2User)token.getPrincipal();
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException {
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken)authentication; // 토큰
+        CustomOAuth2User customOAuth2User = (CustomOAuth2User)token.getPrincipal();
 
-		if (customOAuth2User.getUserType() == UserType.MEMBER) {
-			Member member = memberService.findByUserId(customOAuth2User.getUserId());
-			log.debug("Member 생성 확인: {}", member);
-			if (!member.getAdditionalInfoCompleted()) { // 추가 정보 입력이 필요한 경우
-				// 회원 추가 정보 입력 페이지로 리다이렉트할 URI
-				String memberRedirectUri = UriComponentsBuilder.fromUriString(LOGIN_REDIRECT_URI)
-					.queryParam("additionalInfoCompleted", false)
-					.queryParam("role", "member")
-					.toUriString();
-				registerTokens(request, response, customOAuth2User, memberRedirectUri); // 추가 정보 입력 페이지로 리다이렉트
-				return; // 리다이렉트 후 더 이상 진행하지 않도록 return 처리
-			}
+        if (customOAuth2User.getUserType() == UserType.MEMBER) {
+            Member member = memberService.findByUserId(customOAuth2User.getUserId());
+            log.debug("Member 생성 확인: {}", member);
+            if (!member.getAdditionalInfoCompleted()) { // 추가 정보 입력이 필요한 경우
+                // 회원 추가 정보 입력 페이지로 리다이렉트할 URI
+                String memberRedirectUri = UriComponentsBuilder.fromUriString(LOGIN_REDIRECT_URI)
+                    .queryParam("additionalInfoCompleted", false)
+                    .queryParam("role", "member")
+                    .toUriString();
+                registerTokens(request, response, customOAuth2User, memberRedirectUri); // 추가 정보 입력 페이지로 리다이렉트
+                return; // 리다이렉트 후 더 이상 진행하지 않도록 return 처리
+            }
 
-		}
+        }
 
-		if (customOAuth2User.getUserType() == UserType.TRAINER) {
-			Trainer trainer = trainerService.findByUserId(customOAuth2User.getUserId());
-			if (!trainer.getAdditionalInfoCompleted()) { // 추가 정보 입력이 필요한 경우
-				// 트레이너 추가 정보 입력 페이지로 리다이렉트할 URI
-				String trainerRedirectUri = UriComponentsBuilder.fromUriString(LOGIN_REDIRECT_URI)
-					.queryParam("additionalInfoCompleted", false)
-					.queryParam("role", "trainer")
-					.toUriString();
-				registerTokens(request, response, customOAuth2User, trainerRedirectUri); // 추가 정보 입력 페이지로 리다이렉트
-				return;  // 리다이렉트 후 더 이상 진행하지 않도록 return 처리
-			}
-		}
-		registerTokens(request, response, customOAuth2User, REDIRECT_URI); // 메인 페이지로 리다이렉트 (추가 정보 입력 완료)
-	}
+        if (customOAuth2User.getUserType() == UserType.TRAINER) {
+            Trainer trainer = trainerService.findByUserId(customOAuth2User.getUserId());
+            if (!trainer.getAdditionalInfoCompleted()) { // 추가 정보 입력이 필요한 경우
+                // 트레이너 추가 정보 입력 페이지로 리다이렉트할 URI
+                String trainerRedirectUri = UriComponentsBuilder.fromUriString(LOGIN_REDIRECT_URI)
+                    .queryParam("additionalInfoCompleted", false)
+                    .queryParam("role", "trainer")
+                    .toUriString();
+                registerTokens(request, response, customOAuth2User, trainerRedirectUri); // 추가 정보 입력 페이지로 리다이렉트
+                return;  // 리다이렉트 후 더 이상 진행하지 않도록 return 처리
+            }
+        }
+        registerTokens(request, response, customOAuth2User, REDIRECT_URI); // 메인 페이지로 리다이렉트 (추가 정보 입력 완료)
+    }
 
-	private void registerTokens(HttpServletRequest request, HttpServletResponse response,
-		CustomOAuth2User customUser, String redirectUri) throws
-		IOException {
-		String refreshToken = jwtManager.createRefreshToken(customUser.getUserId(),
-			customUser.getUserType().toString());
-		String accessToken = jwtManager.createAccessToken(customUser.getUserId(), customUser.getUserType().toString());
+    private void registerTokens(HttpServletRequest request, HttpServletResponse response,
+        CustomOAuth2User customUser, String redirectUri) throws
+        IOException {
+        String refreshToken = jwtManager.createRefreshToken(customUser.getUserId(),
+            customUser.getUserType().toString());
+        String accessToken = jwtManager.createAccessToken(customUser.getUserId(), customUser.getUserType().toString());
 
-		cookieManager.setCookie(TokenType.ACCESS_TOKEN, accessToken,
-			expirationPolicy.getAccessTokenExpirationTime());
-		cookieManager.setCookie(TokenType.REFRESH_TOKEN, refreshToken,
-			expirationPolicy.getRefreshTokenExpirationTime());
+        cookieManager.setCookie(TokenType.ACCESS_TOKEN, accessToken,
+            expirationPolicy.getAccessTokenExpirationTime());
+        cookieManager.setCookie(TokenType.REFRESH_TOKEN, refreshToken,
+            expirationPolicy.getRefreshTokenExpirationTime());
 
-		getRedirectStrategy().sendRedirect(request, response, redirectUri);
-	}
+        getRedirectStrategy().sendRedirect(request, response, redirectUri);
+    }
 
 }
