faea5b0281f1cbd578465545250e959cbe9b49e0
daydreaam <dayingyeo@naver.com>
2025-04-07 21:10:57 +0900

    chore(gym) : code convention error

--- src/main/java/org/helloworld/gymmate/domain/gym/gymticket/service/GymTicketService.java
+++ src/main/java/org/helloworld/gymmate/domain/gym/gymticket/service/GymTicketService.java
@@ -34,69 +34,69 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class GymTicketService {
-	private final GymTicketRepository gymTicketRepository;
-	private final MemberService memberService;
-	private final TrainerService trainerService;
-	private final GymProductService gymProductService;
-	private final PartnerGymService partnerGymService;
+    private final GymTicketRepository gymTicketRepository;
+    private final MemberService memberService;
+    private final TrainerService trainerService;
+    private final GymProductService gymProductService;
+    private final PartnerGymService partnerGymService;
 
-	@Transactional
-	public Long createTicket(Long userId, Long gymProductId) {
-		// TODO : 캐시로그 남겨야 함
-		Member member = memberService.findByUserId(userId);
-		GymProduct gymProduct = gymProductService.findByGymProductId(gymProductId);
-		isPurchasable(member, gymProduct);
-		member.updateCash(member.getCash() - gymProduct.getGymProductFee());
-		LocalDate startDate = LocalDate.now();
-		GymTicket gymTicket = GymTicketMapper.toEntity(gymProduct, member, startDate);
-		gymTicketRepository.save(gymTicket);
-		return gymTicket.getGymTicketId();
-	}
+    @Transactional
+    public Long createTicket(Long userId, Long gymProductId) {
+        // TODO : 캐시로그 남겨야 함
+        Member member = memberService.findByUserId(userId);
+        GymProduct gymProduct = gymProductService.findByGymProductId(gymProductId);
+        isPurchasable(member, gymProduct);
+        member.updateCash(member.getCash() - gymProduct.getGymProductFee());
+        LocalDate startDate = LocalDate.now();
+        GymTicket gymTicket = GymTicketMapper.toEntity(gymProduct, member, startDate);
+        gymTicketRepository.save(gymTicket);
+        return gymTicket.getGymTicketId();
+    }
 
-	@Transactional(readOnly = true)
-	public GymTicketPurchaseResponse getPurchaseData(Long userId, Long gymProductId) {
-		Member member = memberService.findByUserId(userId);
-		GymProduct gymProduct = gymProductService.findByGymProductId(gymProductId);
-		return GymTicketMapper.toDto(member.getCash(), gymProduct.getGymProductFee());
-	}
+    @Transactional(readOnly = true)
+    public GymTicketPurchaseResponse getPurchaseData(Long userId, Long gymProductId) {
+        Member member = memberService.findByUserId(userId);
+        GymProduct gymProduct = gymProductService.findByGymProductId(gymProductId);
+        return GymTicketMapper.toDto(member.getCash(), gymProduct.getGymProductFee());
+    }
 
-	private void isPurchasable(Member member, GymProduct gymProduct) {
-		if (member.getCash() < gymProduct.getGymProductFee()) {
-			throw new BusinessException(ErrorCode.INSUFFICIENT_CASH);
-		}
-	}
+    private void isPurchasable(Member member, GymProduct gymProduct) {
+        if (member.getCash() < gymProduct.getGymProductFee()) {
+            throw new BusinessException(ErrorCode.INSUFFICIENT_CASH);
+        }
+    }
 
-	@Transactional(readOnly = true)
-	public Page<MemberGymTicketResponse> getMemberTickets(Long memberId, int page, int pageSize) {
-		Pageable pageable = PageRequest.of(page, pageSize);
-		Page<GymTicket> tickets = gymTicketRepository.findByMember_MemberId(memberId, pageable);
+    @Transactional(readOnly = true)
+    public Page<MemberGymTicketResponse> getMemberTickets(Long memberId, int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<GymTicket> tickets = gymTicketRepository.findByMember_MemberId(memberId, pageable);
 
-		Set<Long> partnerGymIds = tickets.stream()
-			.map(GymTicket::getPartnerGymId)
-			.collect(Collectors.toSet());
-		List<PartnerGymNameProjection> gymInfos = partnerGymService.getGymNamesByIds(partnerGymIds);
-		Map<Long, String> gymNameMap = gymInfos.stream()
-			.collect(Collectors.toMap(
-				PartnerGymNameProjection::getPartnerGymId,
-				PartnerGymNameProjection::getGymName
-			));
+        Set<Long> partnerGymIds = tickets.stream()
+            .map(GymTicket::getPartnerGymId)
+            .collect(Collectors.toSet());
+        List<PartnerGymNameProjection> gymInfos = partnerGymService.getGymNamesByIds(partnerGymIds);
+        Map<Long, String> gymNameMap = gymInfos.stream()
+            .collect(Collectors.toMap(
+                PartnerGymNameProjection::getPartnerGymId,
+                PartnerGymNameProjection::getGymName
+            ));
 
-		return tickets.map(ticket -> {
-			String gymName = gymNameMap.get(ticket.getPartnerGymId());
-			return GymTicketMapper.toMemberGymTicketResponse(ticket, gymName);
-		});
-	}
+        return tickets.map(ticket -> {
+            String gymName = gymNameMap.get(ticket.getPartnerGymId());
+            return GymTicketMapper.toMemberGymTicketResponse(ticket, gymName);
+        });
+    }
 
-	@Transactional(readOnly = true)
-	public Page<PartnerGymTicketResponse> getPartnerGymTickets(Long trainerId, int page, int pageSize) {
-		Trainer trainer = trainerService.findByUserId(trainerId);
-		if (!trainer.getIsOwner()) {
-			throw new BusinessException(ErrorCode.USER_NOT_AUTHORIZED);
-		}
-		PartnerGym partnerGym = partnerGymService.getPartnerGymByOwnerId(trainerId);
-		Pageable pageable = PageRequest.of(page, pageSize);
-		return gymTicketRepository.findAllByPartnerGymId(partnerGym.getPartnerGymId(), pageable)
-			.map(GymTicketMapper::toPartnerGymTicketResponse);
-	}
+    @Transactional(readOnly = true)
+    public Page<PartnerGymTicketResponse> getPartnerGymTickets(Long trainerId, int page, int pageSize) {
+        Trainer trainer = trainerService.findByUserId(trainerId);
+        if (!trainer.getIsOwner()) {
+            throw new BusinessException(ErrorCode.USER_NOT_AUTHORIZED);
+        }
+        PartnerGym partnerGym = partnerGymService.getPartnerGymByOwnerId(trainerId);
+        Pageable pageable = PageRequest.of(page, pageSize);
+        return gymTicketRepository.findAllByPartnerGymId(partnerGym.getPartnerGymId(), pageable)
+            .map(GymTicketMapper::toPartnerGymTicketResponse);
+    }
 
 }
