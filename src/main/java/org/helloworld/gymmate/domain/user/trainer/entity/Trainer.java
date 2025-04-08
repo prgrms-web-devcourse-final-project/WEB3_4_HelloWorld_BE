5e7310a33b6570e4b66de9e0768528a4f888d3f9
daydreaam <dayingyeo@naver.com>
2025-04-08 12:23:37 +0900

    chore(pt) : tab

--- src/main/java/org/helloworld/gymmate/domain/user/trainer/entity/Trainer.java
+++ src/main/java/org/helloworld/gymmate/domain/user/trainer/entity/Trainer.java
@@ -41,124 +41,123 @@ import lombok.NoArgsConstructor;
 @Builder
 @Table(name = "gymmate_trainer")
 public class Trainer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "trainer_id")
+    private Long trainerId;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "trainer_id")
-	private Long trainerId;
+    @Column(name = "trainer_name")
+    private String trainerName;
 
-	@Column(name = "trainer_name")
-	private String trainerName;
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
 
-	@Column(name = "phone_number", unique = true)
-	private String phoneNumber;
+    @Column(name = "email")
+    private String email;
 
-	@Column(name = "email")
-	private String email;
+    @Convert(converter = GenderTypeConverter.class)
+    @Column(name = "gender")
+    private GenderType genderType;
 
-	@Convert(converter = GenderTypeConverter.class)
-	@Column(name = "gender")
-	private GenderType genderType;
+    @Column(name = "bank")
+    private String bank;
 
-	@Column(name = "bank")
-	private String bank;
+    @Column(name = "account")
+    private String account;
 
-	@Column(name = "account")
-	private String account;
+    @Column(name = "business_number", unique = true)
+    private String businessNumber;
 
-	@Column(name = "business_number", unique = true)
-	private String businessNumber;
+    @Column(name = "business_date")
+    private LocalDate businessDate;
 
-	@Column(name = "business_date")
-	private LocalDate businessDate;
+    @Column(name = "is_owner")
+    private Boolean isOwner;
 
-	@Column(name = "is_owner")
-	private Boolean isOwner;
+    @Column(name = "is_check")
+    private Boolean isCheck;
 
-	@Column(name = "is_check")
-	private Boolean isCheck;
+    @Column(name = "profile_url")
+    private String profileUrl;
 
-	@Column(name = "profile_url")
-	private String profileUrl;
+    @Column(name = "is_account_non_locked")
+    private Boolean isAccountNonLocked;
 
-	@Column(name = "is_account_non_locked")
-	private Boolean isAccountNonLocked;
+    @Column(name = "cash")
+    private Long cash;
 
-	@Column(name = "cash")
-	private Long cash;
+    @Column(name = "score")
+    private Double score; // 리뷰 평점 평균
 
-	@Column(name = "score")
-	private Double score; // 리뷰 평점 평균
+    @Column(name = "intro")
+    private String intro; // 소개글
 
-	@Column(name = "intro")
-	private String intro; // 소개글
+    @Column(name = "career")
+    private String career;
 
-	@Column(name = "career")
-	private String career;
+    @Column(name = "field")
+    private String field;
 
-	@Column(name = "field")
-	private String field;
+    @Column(name = "additionalInfoCompleted")
+    private Boolean additionalInfoCompleted; // 추가 정보 입력 여부
 
-	@Column(name = "additionalInfoCompleted")
-	private Boolean additionalInfoCompleted; // 추가 정보 입력 여부
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gym_id")
+    private Gym gym;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "gym_id")
-	private Gym gym;
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "oauth_id")
+    private Oauth oauth;
 
-	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-	@JoinColumn(name = "oauth_id")
-	private Oauth oauth;
+    @OneToMany(mappedBy = "trainer", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Student> students = new ArrayList<>();
 
-	@OneToMany(mappedBy = "trainer", cascade = CascadeType.ALL, orphanRemoval = true)
-	@Builder.Default
-	private List<Student> students = new ArrayList<>();
+    // ====== Business Logic ======
 
-	// ====== Business Logic ======
+    public void registerTrainerInfo(TrainerRegisterRequest request) { // 나중에 헬스장 추가해야함
+        this.trainerName = request.trainerName();
+        this.phoneNumber = request.phoneNumber();
+        this.email = request.email();
+        this.genderType = GenderType.fromString(request.gender());
+        this.bank = request.bank();
+        this.account = request.account();
+        this.additionalInfoCompleted = true;
+    }
 
-	public void registerTrainerInfo(TrainerRegisterRequest request) { // 나중에 헬스장 추가해야함
-		this.trainerName = request.trainerName();
-		this.phoneNumber = request.phoneNumber();
-		this.email = request.email();
-		this.genderType = GenderType.fromString(request.gender());
-		this.bank = request.bank();
-		this.account = request.account();
-		this.additionalInfoCompleted = true;
-	}
+    public void assignGym(Gym gym) {
+        this.gym = gym;
+    }
 
-	public void assignGym(Gym gym) {
-		this.gym = gym;
-	}
+    public void registerOwnerInfo(OwnerRegisterRequest request) {
+        this.isOwner = true;
+        this.isCheck = true;
+        this.trainerName = request.trainerName();
+        this.phoneNumber = request.phoneNumber();
+        this.email = request.email();
+        this.genderType = GenderType.fromString(request.gender());
+        this.bank = request.bank();
+        this.account = request.account();
+        this.businessNumber = request.businessNumber();
+        this.businessDate = LocalDate.parse(request.businessDate(), DateTimeFormatter.ofPattern("yyyyMMdd"));
+        this.additionalInfoCompleted = true;
+    }
 
-	public void registerOwnerInfo(OwnerRegisterRequest request) {
-		this.isOwner = true;
-		this.isCheck = true;
-		this.trainerName = request.trainerName();
-		this.phoneNumber = request.phoneNumber();
-		this.email = request.email();
-		this.genderType = GenderType.fromString(request.gender());
-		this.bank = request.bank();
-		this.account = request.account();
-		this.businessNumber = request.businessNumber();
-		this.businessDate = LocalDate.parse(request.businessDate(), DateTimeFormatter.ofPattern("yyyyMMdd"));
-		this.additionalInfoCompleted = true;
-	}
+    public void modifyTrainerInfo(TrainerModifyRequest request) {
+        this.trainerName = request.trainerName();
+        this.phoneNumber = request.phoneNumber();
+        this.email = request.email();
+        this.bank = request.bank();
+        this.account = request.account();
+        this.profileUrl = request.profile();
+        this.intro = request.intro();
+        this.career = request.career();
+        this.field = request.field();
+    }
 
-	public void modifyTrainerInfo(TrainerModifyRequest request) {
-		this.trainerName = request.trainerName();
-		this.phoneNumber = request.phoneNumber();
-		this.email = request.email();
-		this.bank = request.bank();
-		this.account = request.account();
-		this.profileUrl = request.profile();
-		this.intro = request.intro();
-		this.career = request.career();
-		this.field = request.field();
-	}
-
-	public void updateTrainerProfile(TrainerProfileRequest request) {
-		this.intro = request.intro();
-		this.career = request.career();
-		this.field = request.field();
-	}
+    public void updateTrainerProfile(TrainerProfileRequest request) {
+        this.intro = request.intro();
+        this.career = request.career();
+        this.field = request.field();
+    }
 }
