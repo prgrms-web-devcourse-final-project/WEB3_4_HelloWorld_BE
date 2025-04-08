0ab6a4895c92f09ebe99bdf0cd8afdf49ae28a7a
dia218 <dia19dev@gmail.com>
2025-04-09 02:11:57 +0900

    docs(user): Write Swagger TrainerController & MemberController

--- src/main/java/org/helloworld/gymmate/domain/user/member/controller/MemberController.java
+++ src/main/java/org/helloworld/gymmate/domain/user/member/controller/MemberController.java
@@ -6,7 +6,6 @@ import org.helloworld.gymmate.domain.user.member.dto.MemberRequest;
 import org.helloworld.gymmate.domain.user.member.dto.MemberResponse;
 import org.helloworld.gymmate.domain.user.member.service.MemberService;
 import org.helloworld.gymmate.security.oauth.entity.CustomOAuth2User;
-import org.helloworld.gymmate.security.oauth.service.CustomOAuth2UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,73 +19,71 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@RequestMapping("/member")
-@Slf4j
+@Tag(name = "트레이너 정보 API", description = "트레이너 or 헬스장 운영자 마이페이지, 트레이너 목록 등")
 @RestController
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
 
-	private final MemberService memberService;
-	private final CustomOAuth2UserService customOAuth2UserService;
+    @Operation(summary = "일반 회원 회원가입", description = "일반 로그인 선택했을 때 회원가입")
+    @PostMapping
+    public ResponseEntity<Long> registerAdditionalInfo(
+        @AuthenticationPrincipal CustomOAuth2User oAuth2User,
+        @Valid @RequestBody MemberRequest request
+    ) {
+        Long memberId = memberService.registerMember(oAuth2User.getUserId(), request);
+        return ResponseEntity.ok(memberId);
+    }
 
-	//회원 추가정보 등록
-	@PostMapping
-	public ResponseEntity<Long> registerAdditionalInfo(
-		@AuthenticationPrincipal CustomOAuth2User oAuth2User,
-		@Valid @RequestBody MemberRequest request
-	) {
-		Long memberId = memberService.registerMember(oAuth2User.getUserId(), request);
-		return ResponseEntity.ok(memberId);
-	}
+    @Operation(summary = "일반 회원 마이페이지 정보 수정", description = "일반 회원이 마이페이지에서 자신의 개인 정보 수정")
+    @PreAuthorize("hasRole('ROLE_MEMBER')")
+    @PutMapping
+    public ResponseEntity<Long> modify(@AuthenticationPrincipal CustomOAuth2User oAuth2User,
+        @RequestPart("memberData") @Valid MemberRequest request,
+        @RequestPart(value = "image", required = false) @ValidImageFile MultipartFile profile
+    ) {
+        return ResponseEntity.ok()
+            .body(memberService.modifyMember(
+                //매개변수 : member id, memberModifyRequest객체, profile
+                oAuth2User.getUserId(), request, profile)
 
-	//멤버 정보 수정
-	// 직원 및 사장 개인정보 수정
-	@PreAuthorize("hasRole('ROLE_MEMBER')")
-	@PutMapping
-	public ResponseEntity<Long> modify(@AuthenticationPrincipal CustomOAuth2User oAuth2User,
-		@RequestPart("memberData") @Valid MemberRequest request,
-		@RequestPart(value = "image", required = false) @ValidImageFile MultipartFile profile
-	) {
-		return ResponseEntity.ok()
-			.body(memberService.modifyMember(
-				//매개변수 : member id, memberModifyRequest객체, profile
-				oAuth2User.getUserId(), request, profile)
+            );
+    }
 
-			);
-	}
+    @Operation(summary = "일반 회원 마이페이지 정보 조회", description = "일반 회원이 마이페이지로 이동했을 때 띄워주는 정보")
+    @GetMapping
+    public ResponseEntity<MemberResponse> getMyInfo(
+        @AuthenticationPrincipal CustomOAuth2User oAuth2User
+    ) {
+        MemberResponse memberResponse = memberService.getMember(oAuth2User.getUserId());
 
-	//회원 정보 조회
-	@GetMapping
-	public ResponseEntity<MemberResponse> getMyInfo(
-		@AuthenticationPrincipal CustomOAuth2User oAuth2User
-	) {
-		MemberResponse memberResponse = memberService.getMember(oAuth2User.getUserId());
+        return ResponseEntity.ok(memberResponse);
+    }
 
-		return ResponseEntity.ok(memberResponse);
-	}
+    @Operation(summary = "일반 회원 계정 삭제", description = "일반 회원이 자신의 계정을 삭제")
+    @DeleteMapping
+    public ResponseEntity<Long> deleteMember(
+        @AuthenticationPrincipal CustomOAuth2User oAuth2User
+    ) {
+        memberService.deleteMember(oAuth2User.getUserId());
 
-	//회원 삭제
-	@DeleteMapping
-	public ResponseEntity<Long> deleteMember(
-		@AuthenticationPrincipal CustomOAuth2User oAuth2User
-	) {
-		memberService.deleteMember(oAuth2User.getUserId());
+        return ResponseEntity.ok(oAuth2User.getUserId());
+    }
 
-		return ResponseEntity.ok(oAuth2User.getUserId());
-	}
-
-	//마이페이지 정보
-	@GetMapping("/check")
-	public ResponseEntity<MemberCheckResponse> check(@AuthenticationPrincipal CustomOAuth2User oAuth2User) {
-		return ResponseEntity.ok()
-			.body(memberService.checkUserType(
-					memberService.findByUserId(oAuth2User.getUserId())
-				)
-			);
-	}
+    @Operation(summary = "일반 or 트레이너 체크", description = "일반 회원인지 트레이너 회원(직원, 사장)인지 반환, 메인페이지 로직에 사용")
+    @GetMapping("/check")
+    public ResponseEntity<MemberCheckResponse> check(@AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+        return ResponseEntity.ok()
+            .body(memberService.checkUserType(
+                    memberService.findByUserId(oAuth2User.getUserId())
+                )
+            );
+    }
 
 }
--- src/main/java/org/helloworld/gymmate/domain/user/trainer/controller/TrainerController.java
+++ src/main/java/org/helloworld/gymmate/domain/user/trainer/controller/TrainerController.java
@@ -27,109 +27,112 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "트레이너 정보 API", description = "트레이너 or 헬스장 운영자 마이페이지, 트레이너 목록 등")
 @RestController
 @RequestMapping("/trainer")
 @RequiredArgsConstructor
 public class TrainerController {
+    private final TrainerService trainerService;
 
-	private final TrainerService trainerService;
+    @Operation(summary = "트레이너 직원 회원가입", description = "선생님 로그인 선택 후 헬스장 직원을 선택했을 때 회원가입")
+    @PreAuthorize("hasRole('ROLE_TRAINER')")
+    @PostMapping
+    public ResponseEntity<Long> register(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+        @RequestBody @Valid TrainerRegisterRequest registerRequest) {
+        return ResponseEntity.ok()
+            .body(trainerService.registerInfoByTrainer(trainerService.findByUserId(customOAuth2User.getUserId()),
+                registerRequest));
+    }
 
-	// 직원 추가 정보 등록
-	@PreAuthorize("hasRole('ROLE_TRAINER')")
-	@PostMapping
-	public ResponseEntity<Long> register(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@RequestBody @Valid TrainerRegisterRequest registerRequest) {
-		return ResponseEntity.ok()
-			.body(trainerService.registerInfoByTrainer(trainerService.findByUserId(customOAuth2User.getUserId()),
-				registerRequest));
-	}
+    @Operation(summary = "트레이너 사장 회원가입", description = "선생님 로그인 선택 후 헬스장 사장님을 선택했을 때 회원가입")
+    @PreAuthorize("hasRole('ROLE_TRAINER')")
+    @PostMapping("/owner")
+    public ResponseEntity<Long> register(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+        @RequestBody @Valid OwnerRegisterRequest registerRequest) {
+        return ResponseEntity.ok()
+            .body(trainerService.registerInfoByOwner(trainerService.findByUserId(customOAuth2User.getUserId()),
+                registerRequest));
+    }
 
-	// 사장 추가 정보 등록
-	@PreAuthorize("hasRole('ROLE_TRAINER')")
-	@PostMapping("/owner")
-	public ResponseEntity<Long> register(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@RequestBody @Valid OwnerRegisterRequest registerRequest) {
-		return ResponseEntity.ok()
-			.body(trainerService.registerInfoByOwner(trainerService.findByUserId(customOAuth2User.getUserId()),
-				registerRequest));
-	}
+    @Operation(summary = "트레이너 마이페이지 정보 수정", description = "트레이너 직원 또는 사장이 마이페이지에서 자신의 개인 정보 수정")
+    @PreAuthorize("hasRole('ROLE_TRAINER')")
+    @PutMapping("/modify")
+    public ResponseEntity<Long> modify(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+        @RequestPart("request") TrainerModifyRequest modifyRequest,
+        @RequestPart(value = "image", required = false) @ValidImageFile MultipartFile profile) {
+        return ResponseEntity.ok()
+            .body(trainerService.modifyTrainerInfo(trainerService.findByUserId(customOAuth2User.getUserId()),
+                modifyRequest, profile));
+    }
 
-	// 직원 및 사장 개인정보 수정
-	@PreAuthorize("hasRole('ROLE_TRAINER')")
-	@PutMapping("/modify")
-	public ResponseEntity<Long> modify(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@RequestPart("request") TrainerModifyRequest modifyRequest,
-		@RequestPart(value = "image", required = false) @ValidImageFile MultipartFile profile) {
-		return ResponseEntity.ok()
-			.body(trainerService.modifyTrainerInfo(trainerService.findByUserId(customOAuth2User.getUserId()),
-				modifyRequest, profile));
-	}
+    @Operation(summary = "트레이너 계정 삭제", description = "트레이너 직원 또는 사장님이 자신의 계정을 삭제")
+    @PreAuthorize("hasRole('ROLE_TRAINER')")
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        trainerService.deleteTrainer(trainerService.findByUserId(customOAuth2User.getUserId()));
+        return ResponseEntity.ok().build();
+    }
 
-	@PreAuthorize("hasRole('ROLE_TRAINER')")
-	@DeleteMapping
-	public ResponseEntity<Void> delete(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
-		trainerService.deleteTrainer(trainerService.findByUserId(customOAuth2User.getUserId()));
-		return ResponseEntity.ok().build();
-	}
+    // 한줄소개, 경력, 전문 분야 입력 -> 이거 언제 쓰나요
+    @Operation(summary = "???", description = "??? 페이지에서 ???")
+    @PreAuthorize("hasRole('ROLE_TRAINER')")
+    @PutMapping("/profile")
+    public ResponseEntity<Long> updateProfile(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+        @RequestBody TrainerProfileRequest profileRequest) {
+        return ResponseEntity.ok()
+            .body(trainerService.updateTrainerProfile(trainerService.findByUserId(customOAuth2User.getUserId()),
+                profileRequest));
+    }
 
-	// 한줄소개, 경력, 전문 분야 입력
-	@PreAuthorize("hasRole('ROLE_TRAINER')")
-	@PutMapping("/profile")
-	public ResponseEntity<Long> updateProfile(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@RequestBody TrainerProfileRequest profileRequest) {
-		return ResponseEntity.ok()
-			.body(trainerService.updateTrainerProfile(trainerService.findByUserId(customOAuth2User.getUserId()),
-				profileRequest));
-	}
+    @Operation(summary = "트레이너 마이페이지 정보 조회", description = "트레이너 직원 또는 사장이 마이페이지로 이동했을 때 띄워주는 정보")
+    @GetMapping
+    public ResponseEntity<TrainerResponse> getInfo(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        return ResponseEntity.ok()
+            .body(trainerService.getInfo(trainerService.findByUserId(customOAuth2User.getUserId())));
+    }
 
-	// 마이페이지 정보
-	@GetMapping
-	public ResponseEntity<TrainerResponse> getInfo(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
-		return ResponseEntity.ok()
-			.body(trainerService.getInfo(trainerService.findByUserId(customOAuth2User.getUserId())));
-	}
+    @Operation(summary = "사장 여부 체크", description = "트레이너 사장인지 아닌지 반환, 메인페이지 로직에 사용")
+    @GetMapping("/check")
+    public ResponseEntity<TrainerCheckResponse> check(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        return ResponseEntity.ok()
+            .body(trainerService.check(trainerService.findByUserId(customOAuth2User.getUserId())));
+    }
 
-	// 트레이너인지 check -> 메인페이지 활용
-	@GetMapping("/check")
-	public ResponseEntity<TrainerCheckResponse> check(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
-		return ResponseEntity.ok()
-			.body(trainerService.check(trainerService.findByUserId(customOAuth2User.getUserId())));
+    @Operation(summary = "???? 조회", description = "???페이지에서 ???")
+    @GetMapping("/list")
+    @Validated
+    public ResponseEntity<PageDto<TrainerListResponse>> getTrainerList(
+        @RequestParam(defaultValue = "score") String sortOption,
+        @RequestParam(required = false) String searchOption,
+        @RequestParam(defaultValue = "") String searchTerm,
+        @RequestParam(defaultValue = "0") @Min(0) int page,
+        @RequestParam(defaultValue = "10") @Min(1) @Max(50) int pageSize,
+        @RequestParam(required = false, defaultValue = "127.0276") Double x,
+        @RequestParam(required = false, defaultValue = "37.4979") Double y
+    ) {
+        return ResponseEntity.ok(PageMapper.toPageDto(
+            trainerService.getTrainers(sortOption, searchOption, searchTerm, page, pageSize, x, y)));
+    }
 
-	}
-
-	// 트레이너 전체조회 & 검색
-	@GetMapping("/list")
-	@Validated
-	public ResponseEntity<PageDto<TrainerListResponse>> getTrainerList(
-		@RequestParam(defaultValue = "score") String sortOption,
-		@RequestParam(required = false) String searchOption,
-		@RequestParam(defaultValue = "") String searchTerm,
-		@RequestParam(defaultValue = "0") @Min(0) int page,
-		@RequestParam(defaultValue = "10") @Min(1) @Max(50) int pageSize,
-		@RequestParam(required = false, defaultValue = "127.0276") Double x,
-		@RequestParam(required = false, defaultValue = "37.4979") Double y
-	) {
-		return ResponseEntity.ok(PageMapper.toPageDto(
-			trainerService.getTrainers(sortOption, searchOption, searchTerm, page, pageSize, x, y)));
-	}
-
-	// (회원) 트레이너 가까운순 전체조회 & 검색
-	@GetMapping("/list/nearby")
-	@Validated
-	@PreAuthorize("hasRole('ROLE_MEMBER')")
-	public ResponseEntity<PageDto<TrainerListResponse>> getTrainerList(
-		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@RequestParam(required = false) String searchOption,
-		@RequestParam(defaultValue = "") String searchTerm,
-		@RequestParam(defaultValue = "0") @Min(0) int page,
-		@RequestParam(defaultValue = "10") @Min(1) @Max(50) int pageSize
-	) {
-		return ResponseEntity.ok(PageMapper.toPageDto(
-			trainerService.getNearbyTrainers(searchOption, searchTerm, page, pageSize, customOAuth2User)));
-	}
+    @Operation(summary = "???? 조회", description = "???페이지에서 ???")
+    @GetMapping("/list/nearby")
+    @Validated
+    @PreAuthorize("hasRole('ROLE_MEMBER')")
+    public ResponseEntity<PageDto<TrainerListResponse>> getTrainerList(
+        @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+        @RequestParam(required = false) String searchOption,
+        @RequestParam(defaultValue = "") String searchTerm,
+        @RequestParam(defaultValue = "0") @Min(0) int page,
+        @RequestParam(defaultValue = "10") @Min(1) @Max(50) int pageSize
+    ) {
+        return ResponseEntity.ok(PageMapper.toPageDto(
+            trainerService.getNearbyTrainers(searchOption, searchTerm, page, pageSize, customOAuth2User)));
+    }
 }
