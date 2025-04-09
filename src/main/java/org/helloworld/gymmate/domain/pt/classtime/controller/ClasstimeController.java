01f4b7f5cd5d41e0c904d27a83c3e404553f10b2
daydreaam <dayingyeo@naver.com>
2025-04-09 10:36:10 +0900

    docs(pt) : pt related controller swagger update

--- src/main/java/org/helloworld/gymmate/domain/pt/classtime/controller/ClasstimeController.java
+++ src/main/java/org/helloworld/gymmate/domain/pt/classtime/controller/ClasstimeController.java
@@ -8,6 +8,7 @@ import org.helloworld.gymmate.domain.pt.classtime.entity.Classtime;
 import org.helloworld.gymmate.domain.pt.classtime.service.ClasstimeService;
 import org.helloworld.gymmate.security.oauth.entity.CustomOAuth2User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,42 +18,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "수업 가능시간 API", description = "트레이너의 수업 가능시간 등록, 삭제, 조회")
 @RestController
 @RequestMapping("/classtime")
 @RequiredArgsConstructor
 public class ClasstimeController {
-	private final ClasstimeService classtimeService;
+    private final ClasstimeService classtimeService;
 
-	@PostMapping
-	public ResponseEntity<Map<String, Long>> createClasstime(
-		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@RequestBody @Valid ClasstimeRequest request
-	) {
-		Classtime classTime = classtimeService.createClasstime(request, customOAuth2User.getUserId());
-		return ResponseEntity.ok(
-			Map.of("classTimeId", classTime.getClasstimeId()));
-	}
+    @Operation(summary = "트레이너 - 수업 가능시간 생성")
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_TRAINER')")
+    public ResponseEntity<Map<String, Long>> createClasstime(
+        @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+        @RequestBody @Valid ClasstimeRequest request
+    ) {
+        Classtime classTime = classtimeService.createClasstime(request, customOAuth2User.getUserId());
+        return ResponseEntity.ok(
+            Map.of("classTimeId", classTime.getClasstimeId()));
+    }
 
-	@DeleteMapping
-	public ResponseEntity<Void> deleteClasstime(
-		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@RequestParam("dayOfWeek") Integer dayOfWeek,
-		@RequestParam("time") Integer time
-	) {
-		classtimeService.deleteClasstime(dayOfWeek, time, customOAuth2User.getUserId());
-		return ResponseEntity.ok().build();
-	}
+    @Operation(summary = "트레이너 - 수업 가능시간 삭제")
+    @DeleteMapping
+    @PreAuthorize("hasRole('ROLE_TRAINER')")
+    public ResponseEntity<Void> deleteClasstime(
+        @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+        @RequestParam("dayOfWeek") Integer dayOfWeek,
+        @RequestParam("time") Integer time
+    ) {
+        classtimeService.deleteClasstime(dayOfWeek, time, customOAuth2User.getUserId());
+        return ResponseEntity.ok().build();
+    }
 
-	@GetMapping
-	public ResponseEntity<ClasstimesResponse> getClasstimes(
-		@AuthenticationPrincipal CustomOAuth2User customOAuth2User
-	) {
-		return ResponseEntity.ok(
-			classtimeService.getAvailableTimes(customOAuth2User.getUserId())
-		);
-	}
+    @Operation(summary = "수업 가능시간 전체조회")
+    @GetMapping
+    public ResponseEntity<ClasstimesResponse> getClasstimes(
+        @AuthenticationPrincipal CustomOAuth2User customOAuth2User
+    ) {
+        return ResponseEntity.ok(
+            classtimeService.getAvailableTimes(customOAuth2User.getUserId())
+        );
+    }
 
 }
