82844985fbe594eddc3db8ba06929ca6bc6e763b
seeyeon <gulf72pr@swu.ac.kr>
2025-04-09 18:49:37 +0900

    refactor(myself): 3대 측정 등록, 수정, 삭제 시 member recent 기록 갱신 로직 추가

--- src/main/java/org/helloworld/gymmate/domain/myself/bigthree/repository/BigthreeRepository.java
+++ src/main/java/org/helloworld/gymmate/domain/myself/bigthree/repository/BigthreeRepository.java
@@ -1,9 +1,15 @@
 package org.helloworld.gymmate.domain.myself.bigthree.repository;
 
+import java.util.Optional;
+
 import org.helloworld.gymmate.domain.myself.bigthree.entity.Bigthree;
+import org.helloworld.gymmate.domain.user.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BigthreeRepository extends JpaRepository<Bigthree, Long> {
+
+    // 3대 등록을 같은 날 여러번 한 경우, 더 나중에 등록된 기록을 우선
+    Optional<Bigthree> findTopByMemberOrderByDateDescBigthreeIdDesc(Member member);
 }
