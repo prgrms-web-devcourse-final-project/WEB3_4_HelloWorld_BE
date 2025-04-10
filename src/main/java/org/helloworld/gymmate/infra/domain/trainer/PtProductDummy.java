ff4316a6054b9eec8a69bcdaf1f4837248088461
yuunha <h000414@naver.com>
2025-04-10 12:44:02 +0900

    feat(ptproduct):ptproduct 정보,이미지 예시데이터 따로 저장

---
+++ src/main/java/org/helloworld/gymmate/infra/domain/trainer/PtProductDummy.java
@@ -0,0 +1,26 @@
+package org.helloworld.gymmate.infra.domain.trainer;
+
+import lombok.Getter;
+
+@Getter
+public enum PtProductDummy {
+
+    PT_PODUCT_1("근육 증가 벌크업 PT", "웨이트 위주의 고강도 트레이닝, 체중 증가 & 근육량 증진 목표", 30000L),
+    PT_PODUCT_2("다이어트 PT", "체지방 감량에 집중된 유산소+근력 복합 트레이닝, 식단 가이드 포함", 40000L),
+    PT_PODUCT_3("1:1 맞춤 재활 PT", "부상 이후 회복을 위한 저강도 안정성 운동 + 자세 교정 중심", 50000L),
+    PT_PODUCT_4("하체 강화 & 골반 교정 PT", "약한 하체 근육 강화 + 골반 틀어짐 개선 스트레칭 포함", 60000L),
+    PT_PODUCT_5("홈트 입문자를 위한 PT 기초", "운동 기구 없이 체중 활용, 올바른 자세 습득 중심", 50000L),
+    PT_PODUCT_6("체형 교정 & 자세 밸런스", "라운드숄더, 거북목, 틀어진 골반 개선용 스트레칭 + 근육 강화", 70000L),
+    PT_PODUCT_7("시니어 건강관리 PT", "50대 이상을 위한 관절 보호 운동 + 균형감 유지 훈련", 80000L);
+
+    private final String ptProductName;
+    private final String ptProductInfo;
+    private final Long ptProductFee;
+
+    PtProductDummy(String ptProductName, String ptProductInfo, Long ptProductFee) {
+        this.ptProductName = ptProductName;
+        this.ptProductInfo = ptProductInfo;
+        this.ptProductFee = ptProductFee;
+    }
+
+}
---
+++ src/main/java/org/helloworld/gymmate/infra/domain/trainer/PtProductImageDummy.java
@@ -0,0 +1,21 @@
+package org.helloworld.gymmate.infra.domain.trainer;
+
+import java.util.List;
+
+public class PtProductImageDummy {
+    public static final List<String> Pt_PRODUCT_IMAGE_URLS = List.of(
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/8789c776-3f40-4172-9ffe-fed3230e49b1.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/67086ad4-d4ec-42fe-8fcd-3603a89d3c96.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/d736d0e7-b2af-4e9d-86e3-f17402d9ede6.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/db76e3f4-ac5c-466c-b86e-d3448edf9c8a.jpeg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/d5068bf1-d9bd-4167-926c-4a14394b5019.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/a143c6ce-3ddf-4914-9bc6-99f095bc2ec4.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/a029391a-0e96-4046-8619-30eed32d5c3d.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/d0886950-0d55-497c-8fec-2b87d8a4974b.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/b48b2ba3-b107-44a0-9c4a-309e65dfabc3.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/41d475dd-2ae2-4bb2-962f-eb3fffa36b1d.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/7b5017aa-7c47-45d0-a82c-371ebfc8ad4a.jpg",
+        "https://devcouse4-team12-bucket.s3.ap-northeast-2.amazonaws.com/ptProduct/20250410/39875ad8-799a-488e-82a3-eb8b53c81b68.jpg"
+    );
+
+}
